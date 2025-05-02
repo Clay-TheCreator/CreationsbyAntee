ecf1ba24666a0364c38cd54bc27b130a3ce30865
Clay Wells <claywells30@gmail.com>
2025-05-02 12:04:37 -0700

    header change

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <div className="header-container">
             <h1>
                 <span className="header-line">Handcrafted Indigenous Items</span>
-                Custom Cedar Bark, Essential Oils, Jewelry
+                Custom Cedar Bark, Wellness Oils, Jewelry
             </h1>
             <img src="src/assets/grouppicture.jpeg" alt="" className="header-image" />
         </div>
