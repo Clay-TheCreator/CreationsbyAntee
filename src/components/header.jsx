f5df382987dc5157c438fd44ba06780264b74c01
Clay Wells <claywells30@gmail.com>
2025-05-04 20:54:05 -0700

    header update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,27 @@
 function Header() {
-    return (
-        <div className="header-container">
-            <h1>
-                <span className="header-line">Handcrafted Indigenous Items</span>
-                Custom Cedar Bark, Wellness Oils, Jewelry
-            </h1>
-            <img src="src/assets/grouppicture.jpeg" alt="" className="header-image" />
+  return (
+    <div className="header-card">
+      <div className="header-content">
+        <div className="header-text">
+          <h1 className="header-title">Creations by Antee</h1>
+          <h2 className="header-subtitle">Handcrafted Indigenous Items</h2>
+          <p className="header-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos natus
+            sit aliquid delectus pariatur iure deleniti, suscipit accusantium
+            labore, nostrum saepe quidem cumque amet quaerat! Quia, voluptates
+            iusto?
+          </p>
+          {/* <Link to="/about" className="header-link">Read more about us</Link> */}
         </div>
-    )
+        <div className="header-image-wrapper">
+          <img
+            src="src/assets/grouppicture.jpeg"
+            alt="Group"
+            className="header-image"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Header
+export default Header;
